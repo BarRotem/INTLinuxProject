c68063c02d6437b5d9a632117c38439eb320f24e
BarRotem <bar.rotem45@gmail.com>
2024-06-21 17:23:49 +0300

    LinuxProject - Bar Rotem

---
+++ yourSolution.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Create required dir and file within
+mkdir ./secretDir
+touch ./secretDir/.secret
+
+#Change .secret file permissions to satisfy requirements
+chmod 600 ./secretDir/.secret
+
+#Remove malicious directory and its contents
+rm -rf ./maliciousFiles/
+
+#Unlink malicious link
+unlink ./important.link
+
+#Run secret generator. end script.
+/bin/bash ./generateSecret.sh
